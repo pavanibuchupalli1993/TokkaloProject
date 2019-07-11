653b374c4a40ddf7890c4137e5ba0bacfeacdfd7
vineeth0791 <vineethkumar0791@gmail.com>
2019-07-11 10:34:58 +0530

    vineeth changes

---
+++ app/src/main/java/com/ibetter/www/tokkaloproject/vineeth/VineethMainActivity.java
@@ -0,0 +1,14 @@
+package com.ibetter.www.tokkaloproject.vineeth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class VineethMainActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this,"Vineeth activity",Toast.LENGTH_LONG).show();
+    }
+}
